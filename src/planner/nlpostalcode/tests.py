2f5873cbae440f69b1030ae58b9e644b10ba7781
metin yilmaz <metin.yilmaz50@gmail.com>
2012-11-28 16:38:25 +0100

    Added postalcode test: todo fixture for postalcodes

--- src/planner/nlpostalcode/tests.py
+++ src/planner/nlpostalcode/tests.py
@@ -6,11 +6,16 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.test.client import Client
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    
+    def setUp(self):
+        """ sets up a Django test client """
+        self.client = Client()
+        
+        
+    def test_postcode_lookup(self):
+         response = self.client.get("/pc/get/1056ve")
+         assert "Pieter van der Doesstraat" in response.content
+             
