a648cc18c838b7efbf1cf802b7a20fb46137d3b4
Steven L. Speek <slspeek@gmail.com>
2012-12-13 23:12:41 +0100

    Templating

--- src/planner/main/viewers_views.py
+++ src/planner/main/viewers_views.py
@@ -11,22 +11,32 @@ from .forms import CalendarSearchForm, DatePickForm
 from .schedule import get_region, get_total_weight
 
 @group_required('Viewers')
-def overview(request):
+def overview(request, date_iso):
+    if not date_iso:
+        date_iso=tomorrow()
+    range_list = []
+    for counter in range(-2, 4):
+        begin, end = get_date_interval(date_iso, counter)
+        range_list.append((counter, begin, end))
     car_list = Car.objects.all()
     return render_to_response("main/overview.html", 
                               {"title": _("Overview"),
+                               "date_iso": date_iso,
                                "car_list": car_list,
-                               "range": [("-1", _("Last week")),
-                                          ("0", _("This week")),
-                                          ("1", _("Next week"))]})
+                               "range": range_list,
+                               })
     
-    
+def get_date_interval(date_iso, offset):
+    begin_date = get_date_from_iso(date_iso) + datetime.timedelta(weeks=int(offset))
+    end_date = begin_date + datetime.timedelta(weeks=1)
+    return (begin_date, end_date)
+
 @group_required('Viewers')
 def weekview(request, car_id=0 , offset=0, date_iso=""):
     if not date_iso:
         date_iso=tomorrow()
-    begin_date = get_date_from_iso(date_iso) + datetime.timedelta(weeks=int(offset))
-    end_date = begin_date + datetime.timedelta(weeks=1)
+    offset = int(offset)
+    begin_date, end_date = get_date_interval(date_iso, offset)
     queryset = Calendar.objects.filter(car__pk=int(car_id))
     calendars = queryset.filter(date__range=[begin_date, end_date]).all()
     for cal in calendars:
@@ -40,6 +50,8 @@ def weekview(request, car_id=0 , offset=0, date_iso=""):
                                {"object_list": calendars,
                                 "from": begin_date,
                                 "to": end_date,
+                                "next": offset + 1,
+                                "prev": offset - 1,
                                 "car":car})
 
 
@@ -81,15 +93,20 @@ def render_appointment_list(request, calendar_id):
 @group_required('Viewers')
 def calendar_search_view(request):
     search_results = []
+    result_count = 0
     if not request.POST:
         search_form = CalendarSearchForm()
+        searched = False
     else:
         search_form = CalendarSearchForm(request.POST)
         if search_form.is_valid():
-            search_results = Appointment.objects.filter(customer__name__icontains=search_form.cleaned_data['name'], status=1)
-    logging.error("No of search results %d" % len(search_results) )
+            search_results = Appointment.actives.filter(customer__name__icontains=search_form.cleaned_data['name'])
+            searched = True
+            result_count = len(search_results)
     return render_to_response('calendar_search_view.html',
                               {"search_form": search_form,
+                               "searched": searched,
+                               "result_count": result_count,
                                "search_results": search_results,
                                "title": _("Customer search")},
                               context_instance=RequestContext(request))
