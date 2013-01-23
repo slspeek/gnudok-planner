2d65de9c3bbe27d699c65bf19bc781bfa95c904a
Steven L. Speek <slspeek@gmail.com>
2013-01-23 19:30:05 +0100

    Messages in UI for ajax calls

--- src/planner/main/static/appointment_manipulation.js
+++ src/planner/main/static/appointment_manipulation.js
@@ -1,15 +1,27 @@
 "use strict";
 
-var callout = function() {
-	$('#system-message').text("Calling remote ...").show();	
+var callout_available_dates = function() {
+	$('#date-search').show();	
+};
+var callout_known_customers = function() {
+	$('#customer-search').show();	
+};
+var callout_postcode = function() {
+	$('#postcode-search').show();	
 };
 
-var call_returned = function() {
-	$('#system-message').hide();	
+var call_returned_available_dates = function() {
+	$('#date-search').hide();	
+};
+var call_returned_known_customers = function() {
+	$('#customer-search').hide();	
+};
+var call_returned_postcode = function() {
+	$('#postcode-search').hide();	
 };
 
 var set_customer_data = function(data) {
-	call_returned();
+	call_returned_known_customers();
 	$('#id_town').val(data.town);
 	$('#id_address').val(data.address);
 	$('#id_phone').val(data.phone);
@@ -20,24 +32,24 @@ var set_customer_data = function(data) {
 };
 
 var reset_customer_id = function() {
-	call_returned();
+	call_returned_known_customers();
 	$('#id_found_customer_id').val('');
 };
 
 
 var find_customer = function() {
 	var postcode = get_normalized_postcode();
-	var number = $('#id_number')[0].value
-	var addition = $('#id_additions')[0].value
+	var number = $('#id_number')[0].value;
+	var addition = $('#id_additions')[0].value;
 
 	if (postcode.length === 6) {
-		callout();
+		callout_known_customers();
 		$.getJSON('/main/get_customer/' + postcode + '/' + number + '/' + addition, set_customer_data).error(reset_customer_id);
 	}
 }
 
 var get_normalized_postcode = function() {
-	var postcode = $('#id_postcode')[0].value
+	var postcode = $('#id_postcode')[0].value;
 	return postcode.replace(/ /g, '').toUpperCase();
 	
 };
@@ -45,10 +57,15 @@ $(function() {
 	$("#id_postcode").keyup(function(e) {
 		var postcode = get_normalized_postcode();
 		if (postcode.length === 6) {
+			callout_postcode();
 			$.getJSON('/pc/get/' + postcode, function(data) {
+				call_returned_postcode();
 				$('#id_town').val(data.town);
 				$('#id_address').val(data.address);
 				$('#id_postcode').val(postcode.toUpperCase());
+			}).error( 
+			function(error) {
+				call_returned_postcode();
 			});
 		}
 	});
@@ -56,8 +73,10 @@ $(function() {
 		var unresticted = $('#id_unrestricted').prop('checked');
 		if (unresticted === false) {
 			clear_data();
+			get_updates();
+		} else {
+			get_updates_unrestricted();			
 		}
-		get_updates();
 	});
 	$("#id_postcode").keyup(function(e) {
 		get_updates();
@@ -76,6 +95,6 @@ $(function() {
 	});
 	get_updates();
 	$("#id_postcode").focus();
-	$('#id_stuff').css('width', '90%')
-	$('#id_notes').css('width', '90%')
+	$('#id_stuff').css('width', '90%');
+	$('#id_notes').css('width', '90%');
 });
