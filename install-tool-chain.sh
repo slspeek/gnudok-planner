fc2f4b26c27b3171279c6555c6c91d1c396f2dac
Steven Speek <slspeek@gmail.com>
2013-01-23 13:36:19 +0100

    virtualenv in the requirements

--- install-tool-chain.sh
+++ install-tool-chain.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install --yes --force-yes mysql-server build-essential python2.6-dev
+sudo apt-get install --yes --force-yes mysql-server build-essential python2.6-dev python-virtualenv
 sudo mysqladmin -u root password root
