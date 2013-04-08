2de621de3c5f35ae8d82f671006e8a7d64a18bb7
Steven Speek <slspeek@gmail.com>
2013-04-08 12:52:16 +0200

    Refactored area.get_region_for_postalcode

--- src/planner/area/test/test_get_region_for_postalcode.py
+++ src/planner/area/test/test_get_region_for_postalcode.py
@@ -1,50 +1,56 @@
 from __future__ import absolute_import
-from django.test.client import Client
-from django.test.testcases import TestCase
+from unittest import TestCase
 from planner.main.test.__init__ import RegionFactory
 from nose.plugins.attrib import attr
-from planner.area.models import Interval
+from planner.area.models import Interval, Region
+from planner.area.views import get_region_for_postcalcode
 
 
 @attr('functional', 'postalcode')
 class GetRegionFromPostalcodeTest(TestCase):
-    fixtures = ['test_data.json']
 
     def setUp(self):
         """ sets up a Django test client """
-        self.client = Client()
         self.region = RegionFactory(name='Zuid-Oost', description='Zuid-Oost')
+        self.region_east = RegionFactory(name='Oost', description='Oost')
         self.interval = Interval(begin='1000ab',
                                  end='1001sd',
                                  region=self.region)
         self.interval.save()
+        self.interval_east = Interval(begin='1001aa',
+                                 end='1001sd',
+                                 region=self.region_east)
+        self.interval_east.save()
+    
+    def tearDown(self):
+        Region.objects.all().delete()
+        
+        
+    def doPostcodeTest(self, code, expected_string):
+        regions = get_region_for_postcalcode(code)
+        assert expected_string in str(regions) 
 
     def testUnknown(self):
         """ tests an unkown postalcode """
-        response = self.client.get("/area/postalcode/5000AD")
-        assert 'Unknown' in response.content
+        self.doPostcodeTest("5000AD", 'Unknown')
+        
 
     def testZuidOost(self):
-        """ tests a known postalcode """
-        response = self.client.get("/area/postalcode/1000AC")
-        assert 'Zuid-Oost' in response.content
+        """ tests a known postalcode in Zuid-Oost """
+        self.doPostcodeTest("1000AC", 'Zuid-Oost')
 
     def testZuidOost2(self):
-        """ tests a known postalcode """
-        response = self.client.get("/area/postalcode/1000AB")
-        assert 'Zuid-Oost' in response.content
+        """ tests a known postalcode in Zuid-Oost 2"""
+        self.doPostcodeTest("1000AB", 'Zuid-Oost')
 
     def testZuidOost3(self):
-        """ tests a known postalcode """
-        response = self.client.get("/area/postalcode/1000SD")
-        assert 'Zuid-Oost' in response.content
+        """ tests a known postalcode in Zuid-Oost 3"""
+        self.doPostcodeTest("1000SD", 'Zuid-Oost')
 
     def testZuidOost4(self):
-        """ tests a known postalcode """
-        response = self.client.get("/area/postalcode/1000sd")
-        assert 'Zuid-Oost' in response.content
+        """ tests a known postalcode in Zuid-Oost small case"""
+        self.doPostcodeTest("1000sd", 'Zuid-Oost')
 
     def testZuidOost5(self):
-        """ tests a known postalcode """
-        response = self.client.get("/area/postalcode/1000ac")
-        assert 'Zuid-Oost' in response.content
+        """ tests a known postalcode in Zuid-Oost small case 2"""
+        self.doPostcodeTest("1000ac", 'Zuid-Oost')
