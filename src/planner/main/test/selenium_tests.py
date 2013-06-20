bc00e3575b06c3cba79a12684725478eea308f7a
Zouhair Bakkali <Zouhajr.bakkali@gmail.com>
2013-06-20 13:48:59 +0200

    got postalcode and street

--- src/planner/main/test/selenium_tests.py
+++ src/planner/main/test/selenium_tests.py
@@ -438,6 +438,7 @@ class PostcodeAdmin(DjangoSeleniumTest):
         self.driver.find_element_by_link_text("Nlpostalcode").click()
         self.sleep()
 
+        #Source
         driver = self.driver
         driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[6]").click()
         self.sleep();
@@ -456,8 +457,10 @@ class PostcodeAdmin(DjangoSeleniumTest):
         driver.find_element_by_id("id_ip").clear()
         driver.find_element_by_id("id_ip").send_keys("123.123.123.123")
         driver.find_element_by_name("_save").click()
+        self.sleep()
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Angelique[\s\S]*$")
-
+        
+        #Country
         driver.get(self.live_server_url + "/admin/nlpostalcode/")
         driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[3]").click()
         self.sleep();
@@ -481,7 +484,7 @@ class PostcodeAdmin(DjangoSeleniumTest):
         
         ############################################################################################################
         
-
+        #Province
         driver.get(self.live_server_url + "/admin/nlpostalcode/")
         driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[5]").click()
         self.sleep();
@@ -495,14 +498,13 @@ class PostcodeAdmin(DjangoSeleniumTest):
         driver.find_element_by_id("id_name").send_keys("Noord Holland")
         select_source_elem = driver.find_element_by_id("id_source")
         select_source_elem.send_keys("Source: 1")
-        
         select_country_elem = driver.find_element_by_id("id_country")
         select_country_elem.send_keys("North Korea")
         driver.find_element_by_name("_save").click()
         self.sleep()
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Noord Holland[\s\S]*$")
 
-        
+        #City
         driver.get(self.live_server_url + "/admin/nlpostalcode/")
         driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[2]").click()
         self.sleep();
@@ -520,7 +522,7 @@ class PostcodeAdmin(DjangoSeleniumTest):
         self.sleep()
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*No given name[\s\S]*$")
 
-        
+        #Citynames
         driver.get(self.live_server_url + "/admin/nlpostalcode/")
         driver.find_element_by_link_text("Toevoegen").click()
         self.sleep();
@@ -540,5 +542,45 @@ class PostcodeAdmin(DjangoSeleniumTest):
         self.sleep()      
         self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Amsterdam[\s\S]*$")
         
+        #Postcode
+        driver.get(self.live_server_url + "/admin/nlpostalcode/")
+        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[4]").click()
+        driver.find_element_by_id("id_id").clear()
+        driver.find_element_by_id("id_id").send_keys("1")
+        driver.find_element_by_link_text("Vandaag").click()
+        driver.find_element_by_link_text("Nu").click()
+        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
+        select_source_elem = driver.find_element_by_id("id_source")
+        select_source_elem.send_keys("Source: 1")
+        driver.find_element_by_id("id_fourpp").clear()
+        driver.find_element_by_id("id_fourpp").send_keys("666")
+        select_city_elem = driver.find_element_by_id("id_city")
+        select_city_elem.send_keys("Amsterdam")
+        driver.find_element_by_name("_save").click()
+        self.sleep()
+        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*666[\s\S]*$")
+
         
+        #Street
+        driver.get(self.live_server_url + "/admin/nlpostalcode/")
+        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[7]").click()
+        driver.find_element_by_id("id_id").clear()
+        driver.find_element_by_id("id_id").send_keys("1")
+        driver.find_element_by_link_text("Vandaag").click()
+        driver.find_element_by_link_text("Nu").click()
+        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
+        select_source_elem = driver.find_element_by_id("id_source")
+        select_source_elem.send_keys("Source: 1")
+        driver.find_element_by_id("id_street").clear()
+        driver.find_element_by_id("id_street").send_keys("Axis of Evil Lane")
+        driver.find_element_by_id("id_chars").clear()
+        driver.find_element_by_id("id_chars").send_keys("BZ")
+        select_city_elem = driver.find_element_by_id("id_postcode")
+        select_city_elem.send_keys("666 Amsterdam")
+        driver.find_element_by_name("_save").click()
+        self.sleep()
+        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Axis of Evil Lane[\s\S]*$")
+
         
