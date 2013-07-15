30fd54ad5a0070e63c8a740c7883bb7a4e28882d
Steven Speek <slspeek@gmail.com>
2013-07-15 14:59:20 +0200

    Moved zouhairs selenium test to nlpostcode

--- src/planner/main/test/selenium_tests.py
+++ src/planner/main/test/selenium_tests.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from django.test import LiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+
 from selenium.webdriver.support.ui import Select
 from django.core.urlresolvers import reverse
 
@@ -10,12 +10,13 @@ import logging
 import datetime
 from .__init__ import RegionFactory, TimeSlotFactory, CarFactory, RuleFactory, CalendarFactory
 from .__init__ import CustomerFactory, AppointmentFactory
-import os
+
 from nose.plugins.attrib import attr
 from .__init__ import createRootUser, createTestUsers, createRegion, createRegionEast, createTestPostcodes, adaMakesAppointment, adaMakesBigAppointment
 from planner.main.models import Customer, Calendar
 from planner.nlpostalcode.models import Source, Country, Province, City, Cityname, Postcode, Street
 from planner.main.viewers_views import calendar_search_view
+from planner.testutil.tests import DjangoSeleniumTest
 
 VRIJDAG_11JAN = "11 Jan : Vrijdag :  13:00 - 16:30 - Auto Zeeburg"
 OPHAALDAG = 'Ophaal lijst per dag'
@@ -24,64 +25,7 @@ DO_24JAN = "24 Jan : Donderdag : 9:00 - 12:30 - Auto Zeeburg"
 
 ZUID_OOST = "Zuid-Oost: Zuid-Oost"
 
-class DjangoSeleniumTest(LiveServerTestCase):
-    """ Base class for the django selenium testing """
 
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = WebDriver()
-        super(DjangoSeleniumTest, cls).setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        super(DjangoSeleniumTest, cls).tearDownClass()
-        cls.driver.quit()
-     
-    def tearDown(self):
-        Source.objects.all().delete()
-        Country.objects.all().delete()
-        Province.objects.all().delete()
-        Cityname.objects.all().delete()
-        City.objects.all().delete()
-        Street.objects.all().delete()
-        Postcode.objects.all().delete()
-        super(DjangoSeleniumTest, self).tearDown()
-           
-    def sleep(self):
-        amount = float(os.environ.get("TEST_PAUSE", failobj=2))
-        logging.error("Sleeping for %d seconds" % amount)
-        time.sleep(amount)
-
-    def set_text_field(self, field_id, value):
-        self.driver.find_element_by_id(field_id).clear()
-        self.driver.find_element_by_id(field_id).send_keys(value)
-    
-    def set_select_field(self, field_id, value):
-        Select(self.driver.find_element_by_id(field_id)).select_by_visible_text(value)
-    
-    def assertBobyContains(self, text):
-        self.assertRegexpMatches(self.driver.find_element_by_css_selector("BODY").text, text)
-    
-    def clickPrimairyButton(self):
-        self.driver.find_element_by_css_selector("button.btn.btn-primary").click()
-        self.sleep()
-    
-    def logout(self):
-        self.driver.get(self.live_server_url + reverse('Logout'))
-        self.sleep()
-        
-    def login(self, username, password):
-        #self.driver.get(self.live_server_url + "/accounts/login/?next=" + next)
-        self.go_to_view('Login')
-        self.sleep()
-        self.set_text_field('id_username', username)
-        self.set_text_field('id_password', password)
-        self.driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        self.sleep()
-    
-    def go_to_view(self, view, args=None, kwargs=None):
-        self.driver.get(self.live_server_url + reverse(view, args=args, kwargs=kwargs))
- 
 @attr('selenium', 'search') 
 class SearchTest(DjangoSeleniumTest):
 
@@ -415,176 +359,5 @@ class AppointmentEditMultipleRegions(DjangoSeleniumTest):
         self.assertBobyContains("Ada Lovelace")
         self.assertBobyContains("Bed, boeken en servies")
         self.assertBobyContains("24 januari")
-
-###################################################################
-###################################################################
-@attr('selenium', 'zouhair')
-class PostcodeAdmin(DjangoSeleniumTest):
-    """ Hello selenium """
-    
-    def setUp(self):
-        super(DjangoSeleniumTest, self).setUp()
-        createRootUser(self)
-        
-        
-    def test_logon_as_root_in_admin(self):
-        self.driver.get(self.live_server_url + "/admin/")
-        self.sleep();
-        self.driver.find_element_by_id("id_username").clear()
-        self.driver.find_element_by_id("id_username").send_keys("root")
-        self.driver.find_element_by_id("id_password").clear()
-        self.driver.find_element_by_id("id_password").send_keys("root")
-        self.driver.find_element_by_css_selector("input[type=\"submit\"]").click()
-        #self.driver.find_element_by_link_text("Afmelden").click()
-        self.sleep()
-        self.driver.find_element_by_link_text("Nlpostalcode").click()
-        self.sleep()
-
-        #Source
-        driver = self.driver
-        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[6]").click()
-        self.sleep();
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("1")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        driver.find_element_by_id("id_active").clear()
-        driver.find_element_by_id("id_active").send_keys("1")
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("Angelique")
-        driver.find_element_by_id("id_source").clear()
-        driver.find_element_by_id("id_source").send_keys("1")
-        driver.find_element_by_id("id_ip").clear()
-        driver.find_element_by_id("id_ip").send_keys("123.123.123.123")
-        driver.find_element_by_name("_save").click()
-        self.sleep()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Angelique[\s\S]*$")
-        
-        #Country
-        driver.get(self.live_server_url + "/admin/nlpostalcode/")
-        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[3]").click()
-        self.sleep();
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("1")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        driver.find_element_by_id("id_active").clear()
-        driver.find_element_by_id("id_active").send_keys("1")
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("North Korea")
-        select_source_elem = driver.find_element_by_id("id_source")
-        select_source_elem.send_keys("Source: 1")
-        driver.find_element_by_name("_save").click()
-        self.sleep()
-        
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*North Korea[\s\S]*$")
-        
-        
-        ############################################################################################################
-        
-        #Province
-        driver.get(self.live_server_url + "/admin/nlpostalcode/")
-        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[5]").click()
-        self.sleep();
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("2")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("Noord Holland")
-        select_source_elem = driver.find_element_by_id("id_source")
-        select_source_elem.send_keys("Source: 1")
-        select_country_elem = driver.find_element_by_id("id_country")
-        select_country_elem.send_keys("North Korea")
-        driver.find_element_by_name("_save").click()
-        self.sleep()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Noord Holland[\s\S]*$")
-
-        #City
-        driver.get(self.live_server_url + "/admin/nlpostalcode/")
-        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[2]").click()
-        self.sleep();
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("1")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        select_source_elem = driver.find_element_by_id("id_source")
-        select_source_elem.send_keys("Source: 1")
-        select_country_elem = driver.find_element_by_id("id_province")
-        select_country_elem.send_keys("Noord Holland")
-        driver.find_element_by_name("_save").click()
-        self.sleep()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*No given name[\s\S]*$")
-
-        #Citynames
-        driver.get(self.live_server_url + "/admin/nlpostalcode/")
-        driver.find_element_by_link_text("Toevoegen").click()
-        self.sleep();
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("1")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("Amsterdam")
-        select_source_elem = driver.find_element_by_id("id_source")
-        select_source_elem.send_keys("Source: 1")
-        select_city_elem = driver.find_element_by_id("id_city")
-        select_city_elem.send_keys("No given name")
-        driver.find_element_by_name("_save").click()
-        self.sleep()      
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Amsterdam[\s\S]*$")
-        
-        #Postcode
-        driver.get(self.live_server_url + "/admin/nlpostalcode/")
-        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[4]").click()
-        self.sleep()
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("1")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        select_source_elem = driver.find_element_by_id("id_source")
-        select_source_elem.send_keys("Source: 1")
-        driver.find_element_by_id("id_fourpp").clear()
-        driver.find_element_by_id("id_fourpp").send_keys("666")
-        select_city_elem = driver.find_element_by_id("id_city")
-        select_city_elem.send_keys("Amsterdam")
-        driver.find_element_by_name("_save").click()
-        self.sleep()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*666[\s\S]*$")
-
-        
-        #Street
-        driver.get(self.live_server_url + "/admin/nlpostalcode/")
-        driver.find_element_by_xpath("(//a[contains(text(),'Toevoegen')])[7]").click()
-        self.sleep()
-        driver.find_element_by_id("id_id").clear()
-        driver.find_element_by_id("id_id").send_keys("1")
-        driver.find_element_by_link_text("Vandaag").click()
-        driver.find_element_by_link_text("Nu").click()
-        driver.find_element_by_css_selector("div.form-row.field-updated > div > p.datetime > span.datetimeshortcuts > a").click()
-        driver.find_element_by_xpath("(//a[contains(text(),'Nu')])[2]").click()
-        select_source_elem = driver.find_element_by_id("id_source")
-        select_source_elem.send_keys("Source: 1")
-        driver.find_element_by_id("id_street").clear()
-        driver.find_element_by_id("id_street").send_keys("Axis of Evil Lane")
-        driver.find_element_by_id("id_chars").clear()
-        driver.find_element_by_id("id_chars").send_keys("BZ")
-        select_city_elem = driver.find_element_by_id("id_postcode")
-        select_city_elem.send_keys("666 Amsterdam")
-        driver.find_element_by_name("_save").click()
-        self.sleep()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("BODY").text, r"^[\s\S]*Axis of Evil Lane[\s\S]*$")
 
         
---
+++ src/planner/testutil/tests.py
@@ -0,0 +1,67 @@
+from planner.main.models import Customer, Calendar
+from planner.nlpostalcode.models import Source, Country, Province, City, Cityname, Postcode, Street
+from planner.main.viewers_views import calendar_search_view
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.ui import Select
+from django.core.urlresolvers import reverse
+import os
+import time
+
+class DjangoSeleniumTest(LiveServerTestCase):
+    """ Base class for the django selenium testing """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = WebDriver()
+        super(DjangoSeleniumTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(DjangoSeleniumTest, cls).tearDownClass()
+        cls.driver.quit()
+     
+    def tearDown(self):
+        Source.objects.all().delete()
+        Country.objects.all().delete()
+        Province.objects.all().delete()
+        Cityname.objects.all().delete()
+        City.objects.all().delete()
+        Street.objects.all().delete()
+        Postcode.objects.all().delete()
+        super(DjangoSeleniumTest, self).tearDown()
+           
+    def sleep(self):
+        amount = float(os.environ.get("TEST_PAUSE", failobj=2))
+        time.sleep(amount)
+
+    def set_text_field(self, field_id, value):
+        self.driver.find_element_by_id(field_id).clear()
+        self.driver.find_element_by_id(field_id).send_keys(value)
+    
+    def set_select_field(self, field_id, value):
+        Select(self.driver.find_element_by_id(field_id)).select_by_visible_text(value)
+    
+    def assertBobyContains(self, text):
+        self.assertRegexpMatches(self.driver.find_element_by_css_selector("BODY").text, text)
+    
+    def clickPrimairyButton(self):
+        self.driver.find_element_by_css_selector("button.btn.btn-primary").click()
+        self.sleep()
+    
+    def logout(self):
+        self.driver.get(self.live_server_url + reverse('Logout'))
+        self.sleep()
+        
+    def login(self, username, password):
+        #self.driver.get(self.live_server_url + "/accounts/login/?next=" + next)
+        self.go_to_view('Login')
+        self.sleep()
+        self.set_text_field('id_username', username)
+        self.set_text_field('id_password', password)
+        self.driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+        self.sleep()
+    
+    def go_to_view(self, view, args=None, kwargs=None):
+        self.driver.get(self.live_server_url + reverse(view, args=args, kwargs=kwargs))
+ 
